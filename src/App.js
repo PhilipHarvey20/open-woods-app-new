155d09bc294b47632b14af4e9f5a394347efa389
Philip Harvey <pnharvey12@gmail.com>
2023-05-08 23:45:21 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src="your-logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Your Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+        <Route exact path="/" component={HomePage} />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,227 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import mapboxgl from 'mapbox-gl'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+import CountUp from 'react-countup'
+import { BsPlus, BsDash } from 'react-icons/bs'
+import Select from 'react-select'
+import { stateOptions } from 'web/src/AmericanStates'
+// import './NewHomePage.css'
+
+// import { Form, NumberField, Label, Submit, FieldError } from '@redwoodjs/forms'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+
+import { activityOptions } from 'src/ActivityOptions'
+// import AddressContext from 'src/components/Address_Field/AddressContext'
+import AddressField from 'src/components/Address_Field/AddressField'
+//  ****** MAP ***********
+import ArticlesCell from 'src/components/ArticlesCell'
+import SatelliteMap from 'src/components/Satelite_Map/SateliteMap'
+//  ****** MAP ***********
+import 'web/src/pages/HomePage/HomePage.css'
+
+import CalcPrice from '../../F_CalcPrice/CalculatePriceFunction.js'
+
+const HomePage = () => {
+  // const [create] = useMutation(CREATE_GENERATE_PRICE)
+  const [activityOption, setActivityOption] = useState(null)
+  const [americanState, setAmericanState] = useState(null)
+  const [acreage, setAcreage] = useState(null)
+  const [duration, setDuration] = useState(null)
+  const [finalPrice, setFinalPrice] = useState(null)
+  const [mapData, setMapData] = useState(null)
+
+  const handleClick = () => {
+    CalcThenDisplayPrice()
+    setAddress(address)
+  }
+
+  // *****NEW ADDRESS FILE *****
+  const [address, setAddress] = React.useState('')
+  function handleAddressChange(value) {
+    setAddress(value)
+  }
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    console.log(`Address submitted: ${address}`)
+    // Do something with the address, such as call an API
+  }
+  // *****NEW ADDRESS FILE *****
+
+  const onSubmit = (e) => {
+    // prevent default submit
+    e.preventDefault()
+  }
+  //  ****** MAP ***********
+  // fetch the lng, lat from Mapbox API here
+  const fetchMapData = async (address) => {
+    return mapData
+  }
+  //  ****** MAP ***********
+
+  const CalcThenDisplayPrice = async () => {
+    // const calculatedPrice = CalcPrice('Iowa', 25, 1).toFixed(2)
+    // console log the parameters
+    console.log('activityOption: ', activityOption)
+    console.log('americanState: ', americanState)
+    console.log('acreage: ', acreage)
+    console.log('duration: ', duration)
+    console.log('Address: ', address)
+
+    const calculatedPrice = CalcPrice(americanState, acreage, duration).toFixed(
+      2
+    )
+    console.log('calculatedPrice: ', calculatedPrice)
+    setFinalPrice(calculatedPrice)
+
+    //  ****** MAP ***********
+    if (address) {
+      const mapData = await fetchMapData(address)
+      setMapData(mapData)
+    }
+    //  ****** MAP ***********
+  }
+
+  const incrementAcreage = () => {
+    const newValue = parseFloat(acreage) + 1
+    setAcreage(newValue.toString())
+    onChange(newValue.toString())
+  }
+
+  const decrementAcreage = () => {
+    const newValue = parseFloat(acreage) - 1
+    if (newValue >= 0) {
+      setAcreage(newValue.toString())
+      onChange(newValue.toString())
+    }
+  }
+
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <>
+        <ArticlesCell />
+        <Form onSubmit={onSubmit}>
+          <div className="col-md-6 d-flex ">
+            <div className="w-50">
+              <div className="form-group pl-2 pr-1 dropdown-container">
+                <label
+                  htmlFor="activity"
+                  className=".form-group label {
+"
+                >
+                  Choose an activity
+                </label>
+                <div className="dropdown-button">
+                  <DropdownButton
+                    id="activity-dropdown"
+                    title={
+                      activityOption ? activityOption.label : 'Make a selection'
+                    }
+                    menuVariant="dark"
+                    size="lg"
+                  >
+                    {activityOptions.map((option) => (
+                      <Dropdown.Item
+                        key={option.value}
+                        onClick={() => setActivityOption(option)}
+                      >
+                        {option.label}
+                      </Dropdown.Item>
+                    ))}
+                  </DropdownButton>
+                </div>
+              </div>
+
+              <div className="form-group pl-2 pr-1 dropdown-container">
+                <label htmlFor="state" className="mr-2">
+                  Choose a state
+                </label>
+                <div className="dropdown-button">
+                  <DropdownButton
+                    id="state-dropdown"
+                    title={
+                      americanState ? americanState.label : 'Make a selection'
+                    }
+                    size="lg"
+                  >
+                    <Dropdown.Menu
+                      style={{ maxHeight: '200px', overflowY: 'scroll' }}
+                    >
+                      {stateOptions.map((option) => (
+                        <Dropdown.Item
+                          key={option.value}
+                          onClick={() => setAmericanState(option)}
+                          className={
+                            americanState &&
+                            americanState.value === option.value
+                              ? 'selected-item'
+                              : ''
+                          }
+                        >
+                          {option.label}
+                        </Dropdown.Item>
+                      ))}
+                    </Dropdown.Menu>
+                  </DropdownButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="button-container">
+            <button className="decrement-button" onClick={decrementAcreage}>
+              <BsDash />
+            </button>
+            <input
+              className="input-field"
+              type="number"
+              id="acreage"
+              name="acreage"
+              min="0"
+              value={acreage}
+              onChange={(e) => {
+                setAcreage(e.target.value)
+                onChange(e.target.value)
+              }}
+            />
+            <button className="increment-button" onClick={incrementAcreage}>
+              <BsPlus />
+            </button>
+          </div>
+
+          {/* <div className="form-group col-4 pl-2 pr-1">
+            <label htmlFor="duration">Duration</label>
+            <NumberField
+              className="form-control"
+              name="duration"
+              validation={{ required: true, pattern: { value: /^[1-9]\d*$/ } }}
+              onChange={(e) => setDuration(e.target.value)}
+            />
+          </div> */}
+          <div className="form-group col-4 pl-2 pr-1">
+            <label htmlFor="address">Address</label>
+            <AddressField setAddress={setAddress} />
+          </div>
+          {/* cut here */}
+
+          <div>
+            <p className="final-price">
+              $ <CountUp start={0} end={finalPrice} duration={0.33} />{' '}
+            </p>
+            <button onClick={handleClick}>Submit</button>
+            <></>
+          </div>
+          <div className="map-container">
+            <SatelliteMap address={address} />
+          </div>
+        </Form>
+      </>
+    </>
+  )
+}
+
+export default HomePage
