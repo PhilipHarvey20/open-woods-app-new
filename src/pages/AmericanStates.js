ba1a3ff27d20c905c7a2d058567fb7b7b48c76f3
Philip Harvey <pnharvey12@gmail.com>
2023-05-12 22:51:52 -0400

    added americanState file

--- src/pages/AmericanStates.js
+++ src/pages/AmericanStates.js
@@ -1,83 +1,52 @@
-import React, { useState } from 'react'
-
-import mapboxSdk from '@mapbox/mapbox-sdk'
-import MapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding'
-import './AddressField.css'
-
-const geocodingClient = MapboxGeocoding({
-  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
-})
-
-function AddressField({ setAddress }) {
-  const [inputValue, setInputValue] = useState('')
-  const [suggestions, setSuggestions] = useState([])
-  const [activeOptionIndex, setActiveOptionIndex] = useState(-1)
-
-  async function handleInputChange(event) {
-    const input = event.target.value
-    setInputValue(input)
-
-    const response = await geocodingClient
-      .forwardGeocode({
-        query: input,
-        limit: 5,
-      })
-      .send()
-
-    setSuggestions(response.body.features)
-    setActiveOptionIndex(-1)
-  }
-
-  function handleSuggestionClick(selectedSuggestion) {
-    setAddress(selectedSuggestion.place_name)
-    setInputValue(selectedSuggestion.place_name)
-    setSuggestions([])
-    setActiveOptionIndex(-1)
-  }
-
-  function handleOptionKeyDown(event, index) {
-    if (event.key === 'Enter') {
-      setInputValue(suggestions[index].place_name)
-      setSuggestions([])
-      setActiveOptionIndex(-1)
-    } else if (event.key === 'ArrowUp' && activeOptionIndex > 0) {
-      setActiveOptionIndex(activeOptionIndex - 1)
-    } else if (
-      event.key === 'ArrowDown' &&
-      activeOptionIndex < suggestions.length - 1
-    ) {
-      setActiveOptionIndex(activeOptionIndex + 1)
-    }
-  }
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter an address"
-        autoComplete="off"
-      />
-      {suggestions.length > 0 && (
-        <ul role="listbox">
-          {suggestions.map((suggestion, index) => (
-            <li
-              key={suggestion.id}
-              role="option"
-              aria-selected={activeOptionIndex === index}
-              id={`option-${index}`}
-              tabIndex="0"
-              onClick={() => handleSuggestionClick(suggestion)}
-              onKeyDown={(event) => handleOptionKeyDown(event, index)}
-            >
-              {suggestion.place_name}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
-}
-
-export default AddressField
+export const stateOptions = [
+  { value: 'Alabama', label: 'Alabama' },
+  { value: 'Alaska', label: 'Alaska' },
+  { value: 'Arizona', label: 'Arizona' },
+  { value: 'Arkansas', label: 'Arkansas' },
+  { value: 'California', label: 'California' },
+  { value: 'Colorado', label: 'Colorado' },
+  { value: 'Connecticut', label: 'Connecticut' },
+  { value: 'Delaware', label: 'Delaware' },
+  { value: 'Florida', label: 'Florida' },
+  { value: 'Georgia', label: 'Georgia' },
+  { value: 'Hawaii', label: 'Hawaii' },
+  { value: 'Idaho', label: 'Idaho' },
+  { value: 'Illinois', label: 'Illinois' },
+  { value: 'Indiana', label: 'Indiana' },
+  { value: 'Iowa', label: 'Iowa' },
+  { value: 'Kansas', label: 'Kansas' },
+  { value: 'Kentucky', label: 'Kentucky' },
+  { value: 'Louisiana', label: 'Louisiana' },
+  { value: 'Maine', label: 'Maine' },
+  { value: 'Maryland', label: 'Maryland' },
+  { value: 'Massachusetts', label: 'Massachusetts' },
+  { value: 'Michigan', label: 'Michigan' },
+  { value: 'Minnesota', label: 'Minnesota' },
+  { value: 'Mississippi', label: 'Mississippi' },
+  { value: 'Missouri', label: 'Missouri' },
+  { value: 'Montana', label: 'Montana' },
+  { value: 'Nebraska', label: 'Nebraska' },
+  { value: 'Nevada', label: 'Nevada' },
+  { value: 'New Hampshire', label: 'New Hampshire' },
+  { value: 'New Jersey', label: 'New Jersey' },
+  { value: 'New Mexico', label: 'New Mexico' },
+  { value: 'New York', label: 'New York' },
+  { value: 'North Carolina', label: 'North Carolina' },
+  { value: 'North Dakota', label: 'North Dakota' },
+  { value: 'Ohio', label: 'Ohio' },
+  { value: 'Oklahoma', label: 'Oklahoma' },
+  { value: 'Oregon', label: 'Oregon' },
+  { value: 'Pennsylvania', label: 'Pennsylvania' },
+  { value: 'Rhode Island', label: 'Rhode Island' },
+  { value: 'South Carolina', label: 'South Carolina' },
+  { value: 'South Dakota', label: 'South Dakota' },
+  { value: 'Tennessee', label: 'Tennessee' },
+  { value: 'Texas', label: 'Texas' },
+  { value: 'Utah', label: 'Utah' },
+  { value: 'Vermont', label: 'Vermont' },
+  { value: 'Virginia', label: 'Virginia' },
+  { value: 'Washington', label: 'Washington' },
+  { value: 'West Virginia', label: 'West Virginia' },
+  { value: 'Wisconsin', label: 'Wisconsin' },
+  { value: 'Wyoming', label: 'Wyoming' },
+]
