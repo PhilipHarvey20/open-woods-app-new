ed3e710804cba2fc4abe7186abb11943edacf0a8
Philip Harvey <pnharvey12@gmail.com>
2023-05-15 23:38:09 -0400

    Enlarged navbar and logo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
-
-
 import { Navbar, Nav } from 'react-bootstrap';
+import './App.css'; // Import your custom CSS file for styling
+import HomePage from './pages/HomePage';
 
 function App() {
   return ( 
     <BrowserRouter>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">
-        <img
-      src={process.env.PUBLIC_URL + "/images/OpenWoods_Logo.svg"}
-      width="30"
-      height="30"
-      className="d-inline-block align-top"
-      alt="Your Logo"
-    />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <div className="App">
+        <Navbar bg="dark" variant="dark" expand="lg" className="navbar">
+          <Navbar.Brand href="#home" className="logo-container">
+            <img
+              src={process.env.PUBLIC_URL + "/images/OpenWoods_Logo.svg"}
+              className="logo"
+              alt="Your Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home" className=".nav-link_style">Home</Nav.Link>
+              <Nav.Link href="#about" className="nav-link">About</Nav.Link>
+              <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
