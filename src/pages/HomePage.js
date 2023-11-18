3ad538fa160be4680056538f905e5e41d4e2edff
Philip Harvey <pnharvey12@gmail.com>
2023-11-18 17:00:04 -0500

    Changed background pic and position of form on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { Dropdown, DropdownButton, Button } from 'react-bootstrap'
-import { Form } from 'react-bootstrap'
-import CountUp from 'react-countup'
-import { stateOptions } from './AmericanStates'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import '../pages/HomePage/homepage.css'
-import { activityOptions } from './ActivityOptions'
-import '../components/Address_Field/AddressField.css'
-import SatelliteMap from '../components/Satelite_Map/Satelite_Map'
-import CalcPrice from '../components/F_CalcPrice/CalculatePriceFunction'
+import React, { useState } from "react";
+import { Dropdown, DropdownButton, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import CountUp from "react-countup";
+import { stateOptions } from "./AmericanStates";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "../pages/HomePage/homepage.css";
+import { activityOptions } from "./ActivityOptions";
+import "../components/Address_Field/AddressField.css";
+import SatelliteMap from "../components/Satelite_Map/Satelite_Map";
+import CalcPrice from "../components/F_CalcPrice/CalculatePriceFunction";
 // import { onChange } from 'react-native'
 // import AddressContext from 'src/components/Address_Field/AddressContext'
 // import AddressField from 'src/components/Address_Field/AddressField'
@@ -17,13 +17,12 @@ import CalcPrice from '../components/F_CalcPrice/CalculatePriceFunction'
 // import AddressField from '../components/Address_Field/AddressField';
 
 const HomePage = () => {
-  const [activityOption, setActivityOption] = useState(null)
-  const [americanState, setAmericanState] = useState(null)
-  const [acreage, setAcreage] = useState(null)
-  const [duration, setDuration] = useState(null)
-  const [finalPrice, setFinalPrice] = useState(null)
-  const [mapData, setMapData] = useState(null)
-
+  const [activityOption, setActivityOption] = useState(null);
+  const [americanState, setAmericanState] = useState(null);
+  const [acreage, setAcreage] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [finalPrice, setFinalPrice] = useState(null);
+  const [mapData, setMapData] = useState(null);
 
   // const [activityOption, setActivityOption] = useState({
   //   value: activityOptions[0],
@@ -33,200 +32,199 @@ const HomePage = () => {
   // const [duration, setDuration] = useState(null)
   // const [finalPrice, setFinalPrice] = useState(null)
 
-
-
-
-
   const handleClick = () => {
-    CalcThenDisplayPrice()
-    setAddress(address)
-  }
+    CalcThenDisplayPrice();
+    setAddress(address);
+  };
 
-  const [address, setAddress] = React.useState('')
+  const [address, setAddress] = React.useState("");
   function handleAddressChange(value) {
-    setAddress(value)
+    setAddress(value);
   }
 
   const fetchMapData = async (address) => {
-    return mapData
-  }
+    return mapData;
+  };
 
   const CalcThenDisplayPrice = async () => {
-    console.log('activityOption: ', activityOption)
-    console.log('americanState: ', americanState)
-    console.log('acreage: ', acreage)
-    console.log('duration: ', duration)
-    console.log('Address: ', address)
-    console.log('button submitted')
+    console.log("activityOption: ", activityOption);
+    console.log("americanState: ", americanState);
+    console.log("acreage: ", acreage);
+    console.log("duration: ", duration);
+    console.log("Address: ", address);
+    console.log("button submitted");
 
     // const calculatedPrice = CalcPrice('Iowa', 25, 1).toFixed(2)
-    const calculatedPrice = CalcPrice(americanState.value, acreage, duration).toFixed(2)
-    console.log('calculatedPrice: ', calculatedPrice)
-    setFinalPrice(calculatedPrice)
+    const calculatedPrice = CalcPrice(
+      americanState.value,
+      acreage,
+      duration
+    ).toFixed(2);
+    console.log("calculatedPrice: ", calculatedPrice);
+    setFinalPrice(calculatedPrice);
 
     if (address) {
-      const mapData = await fetchMapData(address)
-      setMapData(mapData)
+      const mapData = await fetchMapData(address);
+      setMapData(mapData);
     }
-  }
+  };
 
   return (
-    <div className ="home-container">
-    <div className="home-input-container">
-      <div className="home-form-container">
-        <div className="home-form">
-          <Form>
-            <div className="form-title">LEASE CALCULATOR</div>
-            <div className="form-inputs">
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <label className="mt-3 mb-3">Choose an Activity</label>
-                <DropdownButton
-                className = "dropdown-button"
-                  id="activity-dropdown"
-                  title={
-                    activityOption ? activityOption.label : 'Make a selection'
-                  }
-                  // menuVariant="dark"
-                  size="lg"
-                  style={{ color: '#778184ff' }} 
-                >
-                  {activityOptions.map((option) => (
-                    <Dropdown.Item
-                      key={option.value}
-                      onClick={() => setActivityOption(option)}
-                    >
-                      {option.label}
-                    </Dropdown.Item>
-                  ))}
-                </DropdownButton>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <label className="mt-3 mb-3">Choose a State</label>
-                <DropdownButton
-                  className = "dropdown-button"
-                  id="state-dropdown"
-                  title={
-                    americanState ? americanState.label : 'Make a selection'
-                  }
-                  size="lg"
+    <div className="home-container">
+      <div className="home-input-container">
+        <div className="home-form-container">
+          <div className="form-title">Outdoor Property Lease Calculator</div>
+          <div className="form-subtitle">
+            {" "}
+            A lease price calculator for outdoor recreational land{" "}
+          </div>
 
-                >
-                  <Dropdown.Menu
-                    style={{ maxHeight: '200px', overflowY: 'scroll' }}
-                  >
-                    {stateOptions.map((option) => (
+          <div className="home-form">
+            <Form>
+              <div className="form-inputs">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}>
+                  <label className="mt-3 mb-3">Choose an Activity</label>
+                  <DropdownButton
+                    className="dropdown-button"
+                    id="activity-dropdown"
+                    title={
+                      activityOption ? activityOption.label : "Make a selection"
+                    }
+                    // menuVariant="dark"
+                    size="lg"
+                    style={{ color: "#778184ff" }}>
+                    {activityOptions.map((option) => (
                       <Dropdown.Item
                         key={option.value}
-                        onClick={() => setAmericanState(option)}
-                        className={
-                          americanState &&
-                            americanState.value === option.value
-                            ? 'selected-item'
-                            : ''
-                        }
-                      >
+                        onClick={() => setActivityOption(option)}>
                         {option.label}
                       </Dropdown.Item>
                     ))}
-                  </Dropdown.Menu>
-                </DropdownButton>
+                  </DropdownButton>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}>
+                  <label className="mt-3 mb-3">Choose a State</label>
+                  <DropdownButton
+                    className="dropdown-button"
+                    id="state-dropdown"
+                    title={
+                      americanState ? americanState.label : "Make a selection"
+                    }
+                    size="lg">
+                    <Dropdown.Menu
+                      style={{ maxHeight: "200px", overflowY: "scroll" }}>
+                      {stateOptions.map((option) => (
+                        <Dropdown.Item
+                          key={option.value}
+                          onClick={() => setAmericanState(option)}
+                          className={
+                            americanState &&
+                            americanState.value === option.value
+                              ? "selected-item"
+                              : ""
+                          }>
+                          {option.label}
+                        </Dropdown.Item>
+                      ))}
+                    </Dropdown.Menu>
+                  </DropdownButton>
+                </div>
+                <div>
+                  <Form.Group
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                    controlId="exampleForm.ControlInput2">
+                    <label className="mt-3 mb-3">Duration (days)</label>
+                    <Form.Control
+                      className="mt-3 mb-3"
+                      style={{ flex: "0 0 40%" }}
+                      type="number"
+                      size="lg"
+                      width={1}
+                      placeholder="Enter duration"
+                      value={duration}
+                      onChange={(e) => setDuration(e.target.value)}
+                    />
+                  </Form.Group>
+                </div>
+
+                <div>
+                  <Form.Group
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                    controlId="exampleForm.ControlInput2">
+                    <label className="mt-3 mb-3">Acreage</label>
+                    <Form.Control
+                      className="mt-3 mb-3"
+                      style={{ flex: "0 0 40%" }}
+                      type="number"
+                      size="lg"
+                      placeholder="Enter acreage"
+                      value={acreage}
+                      onChange={(e) => setAcreage(e.target.value)}
+                    />
+                  </Form.Group>
+                </div>
               </div>
-              <div>
-              <Form.Group 
-              style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-              controlId="exampleForm.ControlInput2">
-                  <label className="mt-3 mb-3">Property Address (optional)</label>
 
-              <Form.Control 
-                   className="mt-3 mb-3"
-                  style={{ flex: '0 0 40%' }}
-                  size='lg'
-                    width={1}
-                    placeholder="Enter address"
-              type="address"  />
-              </Form.Group>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: "1rem",
+                }}>
+                {/* <Button variant="primary" size="lg" color= 'green' onClick={handleClick} */}
 
-
-              </div>
-              <div>
-                <Form.Group
-                  style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-                  controlId="exampleForm.ControlInput2">
-                  <label className="mt-3 mb-3">Duration (days)</label>
-                  <Form.Control
-                  className="mt-3 mb-3"
-                  style={{ flex: '0 0 40%' }}
-                    type="number"
-                    size='lg'
-                    width={1}
-
-                    placeholder="Enter duration"
-                    value={duration}
-                    onChange={(e) => setDuration(e.target.value)}
-                  />
-                </Form.Group>
-              </div>
-              
-              <div>
-                <Form.Group
-                  style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-                  controlId="exampleForm.ControlInput2">
-                  <label className="mt-3 mb-3">Acreage</label>
-                  <Form.Control
-                  className="mt-3 mb-3"
-                  style={{ flex: '0 0 40%' }}
-                    type="number"
-                    size='lg'
-                    placeholder="Enter acreage"
-                    value={acreage}
-                    onChange={(e) => setAcreage(e.target.value)}
-                  />
-                </Form.Group>
-              </div>
-            </div>
-       
-
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1rem'}}>
-            {/* <Button variant="primary" size="lg" color= 'green' onClick={handleClick} */}
-
-      <Button type="submit" 
-      size='lg' onClick={handleClick} 
-      className = "submit-button"
-      >
-      
-        Generate Price
-      </Button>              
-            {/* type='submit' */}
-            {/* className='submit-button'  */}
-            {/* size = 'lg' */}
-            {/* color='submit'           
+                <Button
+                  type="submit"
+                  size="lg"
+                  onClick={handleClick}
+                  className="submit-button">
+                  Generate Price
+                </Button>
+                {/* type='submit' */}
+                {/* className='submit-button'  */}
+                {/* size = 'lg' */}
+                {/* color='submit'           
                 Submit> */}
-      </div>
-      </Form>
-      </div>
-  
-
-      </div>
-      <div className="home-map-container">
+              </div>
+            </Form>
+          </div>
+        </div>
+        {/* <div className="home-map-container">
          <div className=".mapbox-canvas map">
             <SatelliteMap 
             id="map"
             center={[-95.7129, 37.0902]}
             address={address} />
            </div>
+      </div> */}
       </div>
-
-    </div>
-      <div className = "output-container">
+      <div className="output-container">
         <div className="output-box">
-        <label>Final Price: </label>
-        {'$'}
-       <CountUp start={0} end= {finalPrice} duration={0.33} color={'green'} />
+          <label>Final Price: </label>
+          {"$"}
+          <CountUp start={0} end={finalPrice} duration={0.33} color={"green"} />
+        </div>
       </div>
     </div>
-  </div>
-
-
 
     //     <>
     //       {/* <MetaTags title="Home" description="Home page" /> */}
@@ -347,7 +345,7 @@ const HomePage = () => {
     //         </Form>
     //       </>
     //     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
